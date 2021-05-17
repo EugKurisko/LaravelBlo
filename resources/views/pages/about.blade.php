94fd0b592cf02c864dbeeb9202a728c4f9c581f9
Eugen <gram25042002@gmail.com>
2021-05-17 17:43:22 +0300

    updated pages

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+    <p class="mx-auto" style="width: 500px;">First Laravel Project. Here user can register/login/logout.</p>
+    <p class="mx-auto" style="width: 500px;">Every authorized user can add posts, edit, delete own ones</p>
+@endsection
