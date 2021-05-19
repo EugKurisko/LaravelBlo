a661cdf5992f72ba77ebf17b76715d1b57df2ec0
Eugen <gram25042002@gmail.com>
2021-05-19 17:12:53 +0300

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,24 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    @if (!empty($posts))
+                    <h1 class="text-center">Your Posts</h1>
+                        @foreach ($posts as $post)
+                        <div class="card mt-4">
+                            <div class="card-body">
+                                <h1 class="card-title">
+                                    <a class="text-decoration-none" href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                                </h1>
+                                <p class="card-text">
+                                    {{ substr($post->body, 0, 7) . ' ...'}}
+                                </p>
+                                <small>Created at {{ $post->created_at }} by {{ $post->user->name ?? '' }}</small>
+                            </div>
+                        </div>
+                        @endforeach
+                    @else
+                    <h1 class="text-center">There are no posts</h1>
+                    @endif
                 </div>
             </div>
         </div>
