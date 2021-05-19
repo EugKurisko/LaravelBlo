d702d84eb0b586bf1d14fe2da8d8252bb57f38be
Eugen <gram25042002@gmail.com>
2021-05-19 14:53:57 +0300

    user check

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,5 +4,13 @@
     <p>
         {{ $post->body }}
     </p>
-    <small>Created at {{ $post->created_at }} by</small>
+    <small>Created at {{ $post->created_at }} by {{ $post->user->name ?? '' }}</small><br>
+    @if (Auth::user() && Auth::user()->id == $post->user_id)
+    <a class="btn btn-success mt-4" href="{{ $post->id }}/edit">Edit</a>
+    <form action="{{ route('posts.destroy', ['post' => $post]) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button class="btn btn-danger float-right" type="submit">Delete</button>
+    </form>
+    @endif
 @endsection
