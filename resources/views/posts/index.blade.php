20064de7c6f05e647a70a2a02f413794680c0be3
Eugen <gram25042002@gmail.com>
2021-05-21 17:56:57 +0300

    added image display if available and edit/delete buttons is user is author

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,15 +2,43 @@
 @section('content')
     @if (count($posts) > 0)
         @foreach ($posts as $post)
+        
         <div class="card mt-4">
             <div class="card-body">
-                <h1 class="card-title">
-                    <a class="text-decoration-none" href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                </h1>
-                <p class="card-text">
-                    {{ substr($post->body, 0, 7) . ' ...'}}
-                </p>
-                <small>Created at {{ $post->created_at }} by {{ $post->user->name ?? '' }}</small>
+                <div class="row row-eq-height">
+                    @if ($post->image_name)
+                    <div class="col-md-3">
+                        <img class="rounded d-block w-100 h-100" src="{{ url('/images') }}/{{  $post->image_name }}" alt="image" style="width: 100%; height: 500px">
+                    </div>
+                        @endif
+                    <div class="col-md-8" style="height: 100%">
+                        <h1 class="card-title">
+                            <a class="text-decoration-none" href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                        </h1>
+                        <p class="card-text">
+                            @if (strlen($post->body) > 40)
+                                {{ substr($post->body, 0, 40) . "..."}}
+                            @else
+                                {{ $post->body }}
+                            @endif
+                        </p>
+                        <div>
+                            <small>Created at {{ $post->created_at }} by {{ $post->user->name ?? '' }}</small>
+                        </div>
+                    </div>
+                    
+                    @if (Auth::user() && Auth::user()->id == $post->user_id)
+                    <div class="col-md-1" style="height: 100%">
+                        <a class="btn btn-success mt-2  mb-4" href="posts/{{ $post->id }}/edit">Edit</a>
+                        <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-danger float-right" type="submit">Delete</button>
+                        </form>
+                    </div>
+                    @endif
+                
+                </div>
             </div>
         </div>
         @endforeach
