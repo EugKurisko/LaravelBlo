c833ce273a49af5093a303848f6b34b6e5c42c7b
Eugen <gram25042002@gmail.com>
2021-05-21 17:58:39 +0300

    begining of work with comments

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+let btn = document.querySelector('#leaveCom').addEventListener('click', openCommentForm);
+function openCommentForm(e) {
+    e.preventDefault();
+    let form = document.querySelector('#comment');
+    form.classList.toggle('d-none');
+    //form.addEventListener('submit', )
+}
