cf03f8294f501f58b30af7c4160d6c0f73abd6ab
Eugen <gram25042002@gmail.com>
2021-05-26 17:00:56 +0300

    added error-messages display

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,6 +1,13 @@
-@if ($message = Session::get('success'))
-<div id="message" class="alert alert-success alert-block">
-	<button type="button" class="close" data-dismiss="alert">×</button>	
-    <strong>{{ $message }}</strong>
+<div id="message">
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>	
+            <strong>{{ $message }}</strong>
+        </div>
+    @elseif ($message = Session::get('error'))
+        <div class="alert alert-danger alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>	
+            <strong>{{ $message }}</strong>
+        </div>
+    @endif
 </div>
-@endif
