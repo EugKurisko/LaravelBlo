6c15c494c6f95151388f31806469db608e2a64e8
Eugen <gram25042002@gmail.com>
2021-05-26 14:46:38 +0300

    ajax request for writing comments on page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,48 @@
+const { join } = require('lodash');
+
 require('./bootstrap');
 
-let btn = document.querySelector('#leaveCom').addEventListener('click', openCommentForm);
-function openCommentForm(e) {
+var form = $('#comment-form');
+
+$('#leaveCom').on('click', function (e) {
     e.preventDefault();
-    let form = document.querySelector('#comment');
-    form.classList.toggle('d-none');
-    //form.addEventListener('submit', )
-}
+    form.removeClass('d-none');
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': jQuery('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    form.on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/store',
+            type: "post",
+            async: false,
+            data: {
+                comment: jQuery('#comment-text').val(),
+                postId: jQuery('#postId').val(),
+                postAuthor: jQuery('#postAuthor').val(),
+                userName: jQuery('#userName').val(),
+            },
+            success: function (response) {
+                form.addClass('d-none');
+                $('#comment-text').val('');
+                console.log(response);
+                let date = new Date(response.created_at);
+                $('#comments').append('<div class="card mt-4 bg-info"><div class="card-body"><span class="card-title">Written by ' +
+                    response.user.name + ' on '
+                    + date.getFullYear() + '-'
+                    + ("0" + (date.getMonth() + 1)).slice(-2) + '-'
+                    + date.getDate(response.create_at) + ' at '
+                    + (date.getHours() < 10 ? '0' : '') + date.getHours() + ':'
+                    + (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ':'
+                    + (date.getSeconds() < 10 ? '0' : '') + date.getSeconds()
+                    + '</span><div class="card-text">'
+                    + response.comment + '</div>');
+            },
+            error: function (response) {
+                console.log('no');
+            }
+        })
+    })
+})
